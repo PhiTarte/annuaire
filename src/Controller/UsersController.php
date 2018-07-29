8d3909b1da5d3fe48ed25a59b61229434b871173
Unknown <tarteserviceinformatique@sfr.fr>
2018-07-29 11:49:07 +0200

    update UsersController for viewing and adding a user

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,5 +11,28 @@ class UsersController extends AppController
 		$users = $this->Paginator->paginate($this->Users->find());
 		$this->set(compact('users'));
 	}
+	public function view($email = null)
+	{
+		$article = $this->Users->findBySlug($email)->firstOrFail();
+		$this->set(compact('user'));
+	}
+	public function add()
+	{
+		$user = $this->Users->newEntity();
+		if ($this->request->is('post')) {
+			$user = $this->Users->patchEntity($user, $this->request->getData());
+			
+			// Hardcoding the user_id is temporary, and will be removed later
+			// when we build authentication out.
+			$user->id = 1;
+			
+			if ($this->Users->save($user)) {
+				$this->Flash->success(__('Your user has been added'));
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__("Your user can't be added"));
+		}
+		$this->set('user', $user);
+	}
 }
 ?>
